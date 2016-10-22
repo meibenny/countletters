362ba915275534a78c547b6e02b42d2eeed1859b
Benny Mei <meibenny@gmail.com>
2016-10-21 23:51:56 -0400

    made things a little fancier

--- scripts.js
+++ scripts.js
@@ -1,31 +1,28 @@
-
-var myApp = angular.module('LetterCounterApp', []);
+var myApp = angular.module('LetterCounterApp', ['ui.bootstrap']);
 myApp.controller('LetterCounter', ['$scope', function ($scope) {
   $scope.checkboxModel = {
     ignoreCapitals: false
   }
-  $scope.countLetters =
-    function (title = "") {
-      var textIn = title;
-      if ($scope.checkboxModel.ignoreCapitals) {
-        textIn = textIn.toLowerCase();
+
+  $scope.letterCounts = {};
+
+  $scope.countLetters = function (title = '') {
+    var textIn = title;
+    if ($scope.checkboxModel.ignoreCapitals) {
+      textIn = textIn.toLowerCase();
+    }
+    var charCount = {};
+    var i;
+    for (i = 0; i < textIn.length; i++) {
+      if (!(textIn.charAt(i) == ' ')) {
+        charCount[textIn.charAt(i)] = 0;
       }
-      var char_count = new Object();
-      var textIn_length = textIn.length;
-      for (var i = 0; i < textIn_length; i++) {
-        if (!(textIn.charAt(i) == " ")) {
-          char_count[textIn.charAt(i)] = 0;
-        }
+    }
+    for (i = 0; i < textIn.length; i++) {
+      if (!(textIn.charAt(i) == ' ')) {
+        charCount[textIn.charAt(i)] += 1;
       }
-      for (var i = 0; i < textIn_length; i++) {
-        if (!(textIn.charAt(i) == " ")) {
-          char_count[textIn.charAt(i)] += 1;
-        }
-      }
-      textIn = "";
-      for (var eachProperty in char_count) {
-        textIn += eachProperty + " " + char_count[eachProperty] + "\n";
-      }
-      return textIn;
-    };
+    }
+    $scope.letterCounts = charCount;
+  }
 }]);
